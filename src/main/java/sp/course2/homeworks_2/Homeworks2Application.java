ef42bf1cdd4c34d3691e2933efcb28c51a1aa906
PC <ya.brodskii@yandex.ru>
2023-05-09 15:28:54 +0300

    First commit

---
+++ src/main/java/sp/course2/homeworks_2/Homeworks2Application.java
@@ -0,0 +1,13 @@
+package sp.course2.homeworks_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homeworks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homeworks2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sp/course2/homeworks_2/Homeworks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.course2.homeworks_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homeworks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
